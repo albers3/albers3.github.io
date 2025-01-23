9cedc4b904c3a3426c93cebd8ef7cc9681b7daf0
Johann Albers <johannalbers@outlook.de>
2025-01-23 23:37:34 +0100

    feat: add App.vue and main.ts
    Setup App.vue with Banner, Header, RouterView and Footer components.
    Setup main.ts with i18n and 'en' and 'de' locales and a function to load
    the locale messages from the corresponding files.
    
    Signed-off-by: Johann Albers <johannalbers@outlook.de>

---
+++ src/main.ts
@@ -0,0 +1,33 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createI18n } from "vue-i18n"
+import App from './App.vue'
+import router from './router'
+
+function loadLocaleMessages() {
+  const locales = import.meta.glob('./locales/*.json', { eager: true })
+  const messages = {}
+  Object.keys(locales).forEach(key => {
+    const matched = key.match(/([A-Za-z0-9-_]+)\./i)
+    if (matched && matched.length > 1) {
+      const locale = matched[1]
+      messages[locale] = locales[key].default
+    }
+  })
+  return messages
+}
+
+const i18n = createI18n({
+	legacy: false,
+	locale: "en",
+	fallbackLocale: "en",
+	messages: loadLocaleMessages()
+});
+
+const app = createApp(App)
+
+app.use(router)
+app.use(i18n)
+
+app.mount('#app')

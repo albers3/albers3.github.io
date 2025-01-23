08acdffffec8bc706f9514088d6a1eb8400155c0
Johann Albers <johannalbers@outlook.de>
2025-01-23 23:46:28 +0100

    feat: add Banner, Footer and Header components
    Banner component contains coming-soon links for offerings, case-studies
    and capabilities. Banner component contains valid links for the library
    and viewer documentation (semi-valid), source code and bugs/issues.
    
    Signed-off-by: Johann Albers <johannalbers@outlook.de>

---
+++ src/components/Banner.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+</script>
+
+<template>
+<div class="banner">
+	<div class="wrapper">
+		<h5>{{ $t("banner") }} <a class="link inversed" target="_blank" href="https://github.com/albers3/opencae">GitHub</a></h5>
+	</div>
+</div>
+</template>
+
+<style scoped>
+div.banner {
+        padding: 1rem 0;
+        width: 100%;
+        background-color: var(--color-background-dark);
+        color: white;
+}
+
+a.inversed {
+	display: inline-block;
+	color: lightgray;
+	text-decoration-color: gray;
+
+	&:hover {
+		color: white !important;
+		text-decoration-color: white;
+	}
+}
+</style>
